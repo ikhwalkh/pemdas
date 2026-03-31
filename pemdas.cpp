c3a72c826a1363ec50749a0568b175eb07327777
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-03-31 10:48:12 +0700

    bool cekfibonacci(int n) {     int a = 0, b = 1, c;
        while (a <= n) {
            if (a == n) {
                return true;
            }
            c = a + b;
            a = b;
            b = c;
        }
        return false;
    }

--- pemdas.cpp
+++ pemdas.cpp
@@ -22,3 +22,16 @@ bool cekprima(int n){
 }
 
 // function untuk cek fibonacci
+bool cekfibonacci(int n) {
+    int a = 0, b = 1, c;
+
+    while (a <= n) {
+        if (a == n) {
+            return true;
+        }
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return false;
+}
