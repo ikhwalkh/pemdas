80a605e655c241af0f4dbe5758464f526dcb3d55
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-03-31 10:44:26 +0700

    bool cekprima(int n){     if (n <= 1) {         return false;     }
        int i = 2;
        while (i < n) {
            if (n % i == 0) {
                return false;
            }
            i++;
        }
        return true;
    }

--- pemdas.cpp
+++ pemdas.cpp
@@ -6,3 +6,17 @@ int angka;
 int pilihan;
 
 // Function untuk bilangan prima
+bool cekprima(int n){
+    if (n <= 1) {
+        return false;
+    }
+
+    int i = 2;
+    while (i < n) {
+        if (n % i == 0) {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
